d46bdb636e81a81b15b395862cf849873ddc246f
isVertical <xiewendao123@foxmail.com>
2019-05-29 15:43:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ config = {
 		server:"http://rainxn.tt.com",
 		domain:"rainxn.tt.com",
 		project:"default",
-		appid:'152389239297374',
-		secret:'500970dad3324330f8a1085939f20360',
+		appid:'',
+		secret:'',
 		instance:"root",
 	},
 	wxapp:{
 		"cli":"/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli",
-		"appdid": "wx0550a96041cf486c",
-		"secret": "f2e5c4aee55269f427dabc07057d2113"
+		"appdid": "",
+		"secret": ""
 	}
 };
 module.exports = config
